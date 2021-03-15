52680d808bab52acb1afe47fdd360768c18bb5c7
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-15 14:45:48 +0000

    display movements functions added

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-'use strict';
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -61,16 +54,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+const displayMovements=(movements)=>{
+  containerMovements.innerHTML=''
+  movements.forEach((mov,i)=>{
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+   
+   containerMovements.insertAdjacentHTML('afterbegin',html)
+}
+  )
+}
+  
+displayMovements(account1.movements)
